593b951a37e8486862c578d0dc3276c6b70b64e9
c0ny1 <1627639645@qq.com>
2020-10-16 22:25:35 +0800

    添加参数和对应说明绑定器对象

---
+++ src/main/java/me/gv7/woodpecker/plugin/IArgsUsageBinder.java
@@ -0,0 +1,29 @@
+package me.gv7.woodpecker.plugin;
+/*
+ * @(#)IArgsUsageBinder.java
+ *
+ * Copyright woodpecker framework. All rights reserved.
+ *
+ * This code may be used to extend the functionality of woodpecker framework,
+ * provided that this usage does not violate the license terms for those products.
+ */
+import java.util.List;
+
+/**
+ * 该接口绑定参数列表和对应的使用说明
+ */
+public interface IArgsUsageBinder {
+    /**
+     * 设置参数列表
+     *
+     * @param argsList 参数列表
+     */
+    public void setArgsList(List<IArgs> argsList);
+
+    /**
+     * 设置使用说明
+     *
+     * @param usage 使用说明
+     */
+    public void setUsage(String usage);
+}
